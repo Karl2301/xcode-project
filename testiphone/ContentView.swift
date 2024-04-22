a8dc7fcd3542d9394685833fa4f4a24fb64944a7
ItzEniox <crapouille35@icloud.com>
2024-04-22 19:27:38 +0200

    update raciste
    :)

--- testiphone/ContentView.swift
+++ testiphone/ContentView.swift
@@ -4,11 +4,7 @@ struct ContentView: View {
     var body: some View {
         VStack(){
             Image(systemName: "globe")
-<<<<<<< Updated upstream
-            Text("Sale gay")
-=======
-            Text("J'aime")
->>>>>>> Stashed changes
+            Text("Salim igrée")
         }
     }
 }
