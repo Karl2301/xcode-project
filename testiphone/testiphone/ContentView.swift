3b0d2ac2128c0b92e96363704705699c4d6d09cd
ItzEniox <crapouille35@icloud.com>
2024-04-22 19:08:42 +0200

    sq
    sq

--- testiphone/testiphone/ContentView.swift
+++ testiphone/testiphone/ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     var body: some View {
         VStack(){
             Image(systemName: "globe")
-            Text("Bienvenue")
+            Text("J'aime les bites")
         }
     }
 }
