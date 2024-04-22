5a2be8de9b061b99a9d7391203b70c361a621390
ItzEniox <130908560+ItzEniox@users.noreply.github.com>
2024-04-22 18:51:48 +0200

    Add files via upload

---
+++ testiphone/testiphone/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(){
+            Image(systemName: "globe")
+            Text("Bienvenue")
+        }
+    }
+}
+#Preview {
+    ContentView()
+    
+   
+}
---
+++ testiphone/testiphone/testiphoneApp.swift
@@ -0,0 +1,18 @@
+//
+//  testiphoneApp.swift
+//  testiphone
+//
+//  Created by Annaeg on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct testiphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello word")
+            ContentView()
+        }
+    }
+}
