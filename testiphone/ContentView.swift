6cbaeaef383e44f6e74c56e2001d7afc94b44e80
Karl2301 <karl.rahuel@gmail.com>
2024-04-23 19:28:43 +0200

    xcopde

--- testiphone/ContentView.swift
+++ testiphone/ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
         VStack(){
             Image(systemName: "globe")
             Text("Annaeg X Karl")
-            Text("sal enoir")
+            Text("Xcode 2024")
         }
     }
 }
