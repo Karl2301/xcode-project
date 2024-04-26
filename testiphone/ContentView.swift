b350ba3335683a7a5d16bf5fb84d36ba0bb2cd9f
ItzEniox <crapouille35@icloud.com>
2024-04-26 19:21:23 +0200

    Avancement page proposition circuits

--- testiphone/ContentView.swift
+++ testiphone/ContentView.swift
@@ -12,38 +12,117 @@ struct ContentView: View {
         }
         VStack {
             HStack{
-                Text("Circuits")
+                Text("Circuits :")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
                 }
                 Spacer().frame(height: 30)
-                // TODO: your content goes here...
-                VStack(){
+            HStack{
+                Text("Imola")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 80)
+                Text("Redbullring")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 40)
+            }
+            Grid{
+                GridRow(){
                     Button {
-                            
-                        } label: {
-                            Image("imola")
-                                .resizable()
-                                .frame(width: 200, height: 100)
-                                .cornerRadius(8)
-                        }
-                    Spacer().frame(height: 30)
+                                                
+                    } label: {
+                        Image("imola")
+                            .resizable()
+                            .frame(width: 200, height: 100)
+                            .cornerRadius(8)
                     Button {
-                            
-                        } label: {
-                            Image("redbullring")
-                                .resizable()
-                                .frame(width: 200, height: 100)
-                                .cornerRadius(8)
-                        }
-
+                                                    
+                    } label: {
+                        Image("redbullring")
+                            .resizable()
+                            .frame(width: 200, height: 100)
+                            .cornerRadius(8)
+                
+                                                }
+                                            }
+                    }
                 }
+            HStack{
+                Text("Zandvoort")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 58)
+                Text("Spa-Francorchamps")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 0)
+            }
+            Grid{
+                GridRow(){
+                    Button {
+                                                
+                    } label: {
+                        Image("zandvoort")
+                            .resizable()
+                            .frame(width: 200, height: 100)
+                            .cornerRadius(8)
+                    Button {
+                                                    
+                    } label: {
+                        Image("spa")
+                            .resizable()
+                            .frame(width: 200, height: 100)
+                            .cornerRadius(8)
+                                                }
+                                            }
+                    }
+                }
+            HStack{
+                Text("Bathurst")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 59)
+                Text("Watkins glen")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 32)
+            }
+            Grid{
+                GridRow(){
+                    Button {
+                                                
+                    } label: {
+                        Image("bathurst")
+                            .resizable()
+                            .frame(width: 200, height: 100)
+                            .cornerRadius(8)
+                    Button {
+                                                    
+                    } label: {
+                        Image("watkins glen")
+                            .resizable()
+                            .frame(width: 200, height: 100)
+                            .cornerRadius(8)
+                                                }
+                                            }
+                    }
+                }
+            }
+            
+                
                 Spacer() /// forces content to top
             }
     }
-}
+
 #Preview {
     ContentView()
     
