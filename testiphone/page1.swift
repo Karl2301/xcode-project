41e88f289e662d5d833c77ec217a5eb97ac77606
ItzEniox <crapouille35@icloud.com>
2024-04-27 15:51:49 +0200

    ajout de la page 1 (Sélection du jeu)

---
+++ testiphone/page1.swift
@@ -0,0 +1,18 @@
+//
+//  page1.swift
+//  testiphone
+//
+//  Created by Annaeg on 27/04/2024.
+//
+
+import SwiftUI
+
+struct page1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    page1()
+}
