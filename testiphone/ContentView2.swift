908d04b6e23c56eefe9996dbc4ce0784c1444dca
ItzEniox <crapouille35@icloud.com>
2024-04-27 16:29:32 +0200

    ajout contentview2 pour les circuits assetto corsa

---
+++ testiphone/ContentView2.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView2.swift
+//  testiphone
+//
+//  Created by Annaeg on 27/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView2()
+}
